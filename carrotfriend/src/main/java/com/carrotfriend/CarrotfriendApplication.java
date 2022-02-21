467d281242a40d43b0be82c32803d5bd20d2cf55
Garo-o <hjs8482@naver.com>
2022-02-21 21:01:25 +0900

    feat: project settings

---
+++ carrotfriend/src/main/java/com/carrotfriend/CarrotfriendApplication.java
@@ -0,0 +1,13 @@
+package com.carrotfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotfriendApplication.class, args);
+	}
+
+}
---
+++ carrotfriend/src/test/java/com/carrotfriend/CarrotfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrotfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotfriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
