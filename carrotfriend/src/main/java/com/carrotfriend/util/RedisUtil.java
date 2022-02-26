5f1f8618074e90613265c1807eaa15ebc90c4a8c
Garo-o <hjs8482@naver.com>
2022-02-26 23:48:26 +0900

    feat: jwt-privider, jwt-filter

---
+++ carrotfriend/src/main/java/com/carrotfriend/util/RedisUtil.java
@@ -0,0 +1,31 @@
+package com.carrotfriend.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+    private final RedisTemplate<String, Object> template;
+
+    public void set(String key, Object o, long milliseconds){
+        template.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+        template.opsForValue().set(key, o, milliseconds, TimeUnit.MILLISECONDS);
+    }
+
+    public Object get(String key){
+        return template.opsForValue().get(key);
+    }
+
+    public boolean delete(String key){
+        return template.delete(key);
+    }
+
+    public boolean hasKey(String key){
+        return template.hasKey(key);
+    }
+}
