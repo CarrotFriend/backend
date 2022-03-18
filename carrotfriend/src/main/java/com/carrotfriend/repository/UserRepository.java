05deba7cb58130cbcf66f2d94fdab4be75d1b1d7
DESKTOP-TJRFUAU\hong <hjs8482@naver.com>
2022-03-18 13:37:05 +0900

    udpate: repository-user

--- carrotfriend/src/main/java/com/carrotfriend/repository/UserRepository.java
+++ carrotfriend/src/main/java/com/carrotfriend/repository/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUserId(String userId);
-    public User findByEmail(String email);
+    public Optional<User> findByUserId(String userId);
+    public Optional<User> findByEmail(String email);
 }
