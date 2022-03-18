c8ac3bb3b2a3b99e98b1d0ac8e8495bcd22ce5ce
DESKTOP-TJRFUAU\hong <hjs8482@naver.com>
2022-03-18 13:32:55 +0900

    update : jwt

--- carrotfriend/src/main/java/com/carrotfriend/jwt/JwtToken.java
+++ carrotfriend/src/main/java/com/carrotfriend/jwt/JwtToken.java
@@ -1,9 +1,11 @@
 package com.carrotfriend.jwt;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.Date;
 
+@Getter
 @Builder
 public class JwtToken {
     private String grantType;
