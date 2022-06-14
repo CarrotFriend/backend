d05009ca285b657fee208358792d289ea91c8f57
Garo-o <hjs8482@naver.com>
2022-06-15 00:41:36 +0900

    refact: remain dto

--- carrotfriend/src/main/java/com/carrotfriend/dto/post/ImageDto.java
+++ carrotfriend/src/main/java/com/carrotfriend/dto/post/ImageDto.java
@@ -1,15 +1,20 @@
 package com.carrotfriend.dto.post;
 
 import com.carrotfriend.domain.Image;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageDto {
     private Long id;
     private String src;
     public static ImageDto of(Image image){
+        if(image == null) return null;
         return ImageDto.builder()
                 .id(image.getId())
                 .src(image.getSrc())
