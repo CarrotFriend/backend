9611c68616279f1be4ceb05e4304f337f284f589
Garo-o <hjs8482@naver.com>
2022-06-15 00:39:11 +0900

    refact: user logic refactoring

--- carrotfriend/src/main/java/com/carrotfriend/controller/UserController.java
+++ carrotfriend/src/main/java/com/carrotfriend/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.carrotfriend.controller;
 
-import com.carrotfriend.dto.user.CategoryDto;
-import com.carrotfriend.dto.user.JoinDto;
-import com.carrotfriend.dto.user.UserCateDto;
-import com.carrotfriend.dto.user.UserDto;
+import com.carrotfriend.dto.user.*;
 import com.carrotfriend.service.UserService;
 import com.carrotfriend.util.Response;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,33 +20,33 @@ public class UserController {
     private final UserService userService;
     private final Response response;
 
-//    @Operation(summary = "유저 조회", description = "아이디로 1명 조회")
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> findUserById(@Parameter(description = "id", required = true, example = "1") @PathVariable Long id){
-//        return response.success(UserDto.of(userService.findById(id)));
-//    }
     @Operation(summary = "유저 조회", description = "아이디로 1명 조회")
     @GetMapping("/{userId}")
     public ResponseEntity<?> findUserById(@Parameter(description = "id", required = true, example = "username") @PathVariable String userId){
-        return response.success(UserDto.of(userService.findByUserId(userId)));
+        return response.success(UserDto.of(userService.getUserByUserId(userId)));
     }
     @Operation(summary = "회원 가입", description = "")
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody JoinDto user){
-        return response.success(userService.join(user));
+        return response.success(UserDto.of(userService.join(user)));
     }
 
-    @DeleteMapping("/")
+    @GetMapping("/check-list")
+    public ResponseEntity<?> checkList(){
+        return response.success(JoinCheckDto.of(userService.getListForJoin()));
+    }
+    @PutMapping("/modify")
+    public ResponseEntity<?> modifyUser(@RequestBody UpdateDto updateDto){
+        return null;
+    }
+
+    @DeleteMapping("/remove")
     public ResponseEntity<?> deleteUser(@RequestBody UserDto userDto){
         return response.success(userService.deleteUser(userDto));
     }
     @PostMapping("/category")
-    public ResponseEntity<?> addCategory(@RequestBody UserCateDto userCateDto){
-        userService.insertCategory(userCateDto.getUserDto(),userCateDto.getCategoryDto());
+    public ResponseEntity<?> setCategory(@RequestBody UserCateDto userCateDto){
+        userService.setCategory(userCateDto.getUserDto());
         return response.success(Collections.emptyList());
-    }
-    @DeleteMapping("/category")
-    public ResponseEntity<?> deleteCategory(@RequestBody UserCateDto userCateDto){
-        return response.success(userService.deleteCategory(userCateDto.getUserDto(),userCateDto.getCategoryDto()));
     }
 }
--- carrotfriend/src/main/java/com/carrotfriend/service/UserService.java
+++ carrotfriend/src/main/java/com/carrotfriend/service/UserService.java
@@ -1,75 +1,106 @@
 package com.carrotfriend.service;
 
 import com.carrotfriend.domain.Category;
+import com.carrotfriend.domain.Role;
 import com.carrotfriend.domain.User;
 import com.carrotfriend.domain.UserToCategory;
 import com.carrotfriend.dto.user.CategoryDto;
+import com.carrotfriend.dto.user.JoinCheckDto;
 import com.carrotfriend.dto.user.JoinDto;
 import com.carrotfriend.dto.user.UserDto;
+import com.carrotfriend.exception.UserNotFoundException;
 import com.carrotfriend.repository.UserRepository;
 import com.carrotfriend.repository.UserToCategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class UserService {
+    @PersistenceContext
+    private final EntityManager em;
     private final UserRepository userRepository;
-    private final CategoryService categoryService;
     private final PasswordEncoder passwordEncoder;
+    private final CategoryService categoryService;
     private final UserToCategoryRepository userToCategoryRepository;
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public User join(JoinDto joinUser){
+        logger.info("[UserService] join called");
+
         User user = userRepository.save(User.builder()
                 .userId(joinUser.getUserId())
                 .pw(passwordEncoder.encode(joinUser.getPw()))
                 .nickName(joinUser.getNickName())
                 .email(joinUser.getEmail())
+                .role(Role.USER)
+                .regDate(LocalDateTime.now())
+                .image(null)
                 .birthday(joinUser.getBirthday())
-                .userToCategoryList(Collections.emptyList())
                 .build());
         return user;
     }
 
-    public void insertCategory(UserDto userDto, List<CategoryDto> categoryDtos) {
-        User user = findById(userDto.getId());
-        categoryDtos.forEach(categoryDto->{
-            Category category = categoryService.findCategoryByCodeAndName(categoryDto.getCode(), categoryDto.getName());
-            userToCategoryRepository.save(UserToCategory.createRelWithUserAndCat(user, category));
+    public void setCategory(UserDto userDto) {
+        User user = getUserById(userDto.getId());
+        List<UserToCategory> willRemove = new ArrayList<>();
+        List<UserToCategory> willAdd = new ArrayList<>();
+
+        user.getUserToCategoryList().forEach(c->{
+            if(!userDto.getCategoryList().contains(CategoryDto.of(c.getCategory()))) willRemove.add(c);
+            else userDto.getCategoryList().remove(CategoryDto.of(c.getCategory()));
         });
+
+        categoryService.getCategoriesById(userDto.getCategoryList().stream().map(c->c.getId()).collect(Collectors.toList()))
+                .forEach(c->willAdd.add(UserToCategory.createRelWithUserAndCat(user, c)));
+
+        logger.info("willRemove : {}",willRemove);
+        logger.info("willAdd : {}",willAdd);
+
+        willRemove.forEach(c->user.removeCategory(c));
+        userToCategoryRepository.saveAll(willAdd);
     }
 
     public boolean deleteUser(UserDto userDto){
-        userRepository.delete(findByUserId(userDto.getUserId()));
+        userRepository.delete(getUserById(userDto.getId()));
         return true;
     }
 
-    public UserDto deleteCategory(UserDto userDto, List<CategoryDto> categoryDtos){
-        User user = findById(userDto.getId());
-        categoryDtos.forEach(categoryDto->{
-            Category category = categoryService.findOneByCode(categoryDto.getCode());
-            userToCategoryRepository.deleteUserToCategoryByUserIdAndCategoryId(user.getId(), category.getCode());
-        });
-
-        return UserDto.of(findById(userDto.getId()));
+    public User getUserById(Long id){
+        return em.createQuery("select distinct u from User u left join fetch u.userToCategoryList where u.id =:id", User.class)
+                .setParameter("id", id)
+                .getSingleResult();
     }
 
-    public User findById(Long id){
-        User user = userRepository.findById(id).orElseGet(null);
-        if(user == null) throw new UsernameNotFoundException("User Id : "+id+" not exist");
-        return user;
+    public User getUserByUserId(String userId){
+        return em.createQuery("select distinct u from User u left join fetch u.userToCategoryList where u.userId =:userId", User.class)
+                .setParameter("userId", userId)
+                .getSingleResult();
     }
 
-    public User findByUserId(String userId){
-        User user = userRepository.findByUserId(userId).orElse(null);
-        if(user == null) throw new UsernameNotFoundException("Not Found this Id");
-        return user;
+    public User getUserByEmail(String email){
+        return em.createQuery("select distinct u from User u left join fetch u.userToCategoryList where u.email =:email", User.class)
+                .setParameter("email", email)
+                .getSingleResult();
+    }
+
+    public List<User> getListForJoin() {
+        return userRepository.findAll();
+
     }
 }
